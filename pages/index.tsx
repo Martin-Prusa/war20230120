a254f538dea77b2f71ece3983abf39cdada81705
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2023-01-20 09:28:31 +0100

    form

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,43 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import {SetStateAction, useState} from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
+
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    const [password2, setPassword2] = useState('')
+    const [profession, setProfession] = useState('')
+    const [agree, setAgree] = useState(false)
+
+    const handleName = (event: any) => {
+        setName(event.target.value)
+    }
+
+    const handlePassword = (event: any) => {
+        setPassword(event.target.value)
+    }
+
+    const handlePassword2 = (event: any) => {
+        setPassword2(event.target.value)
+    }
+
+    const handleProfession = (event: any) => {
+        setProfession(event.target.value)
+    }
+
+    const handleAgree = (event: any) => {
+        setAgree(event.target.checked)
+    }
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault()
+        alert(`Uživatel ${name} je ${profession} developer. Jeho heslo je ${password}`)
+    }
+
   return (
     <>
       <Head>
@@ -15,108 +48,45 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+            <h1>Form</h1>
+          <form onSubmit={handleSubmit}>
+              <div>
+                  <label>
+                      Name:
+                      <input id="name" type="text" value={name} onChange={handleName}/>
+                  </label>
+              </div>
+              <div>
+                  <label>
+                      Password:
+                      <input id="password" type="password" value={password} onChange={handlePassword}/>
+                  </label>
+              </div>
+              <div>
+                  <label>
+                      Password 2:
+                      <input id="password2" type="password" value={password2} onChange={handlePassword2}/>
+                  </label>
+              </div>
+              <div>
+                  <label>
+                      Profession:
+                      <select id="profession" value={profession} onChange={handleProfession}>
+                          <option value="frontend">Frontend</option>
+                          <option value="backend">Backend</option>
+                      </select>
+                  </label>
+              </div>
+              <div>
+                  <label>
+                      Souhlasím s podmínkami:
+                      <input id="agree" type="checkbox" onChange={handleAgree}/>
+                  </label>
+              </div>
+              <div>
+                  <button disabled={!agree || password !== password2} type="submit">Submit</button>
+              </div>
+          </form>
       </main>
     </>
   )
